a16671fa01a3f8a527fc79d8b5676cdbe90c03d2
9ziggy9 <davidarogers@protonmail.com>
2022-07-27 21:49:12 -0700

    implement resizing with realloc

--- main.c
+++ main.c
@@ -42,7 +42,9 @@ int main(void) {
   Darray_int darr = Darray_int_init(5);
   Darray_int_print(&darr);
   Darray_int_push(&darr, 1);
-  Darray_int_print(&darr);
+  Darray_int_push(&darr, 1);
+  Darray_int_push(&darr, 1);
+  Darray_int_push(&darr, 1);
   Darray_int_destroy(&darr);
   return 0;
 }
