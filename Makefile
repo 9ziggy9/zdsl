5eac1a1ebb02e1d8a7b555721ac0e96fbf6c48e3
9ziggy9 <davidarogers@protonmail.com>
2022-07-27 21:47:59 -0700

    further Darr implementations

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS=-Wall -std=c11 -pedantic -Wextra -ansi -Wconversion
+CFLAGS=-Wall -std=c11 -pedantic -Wextra -Wconversion
 
 all: main.c
 	$(CC) $(CFLAGS) main.c -o test
--- main.c
+++ main.c
@@ -15,20 +15,34 @@ Darray_int Darray_int_init(size_t capacity) {
   return darr;
 }
 
+void Darray_int_destroy(Darray_int* darr) {
+  free(darr->arr);
+}
+
 void Darray_int_print(Darray_int* darr) {
   printf("{");
-  for (int i = 0; i < darr->size; i++) {
-    printf("%d", i);
+  for (size_t i = 0; i < darr->size; i++) {
+    printf("%d", darr->arr[i]);
     printf("%c", i < darr->size - 1 ? ',' : '\0');
   }
   printf("}\n");
 }
 
-void Darray_int_destroy(Darray_int* darr) {
-  free(darr);
+int Darray_int_push(Darray_int* darr, int x) {
+  if ((darr->size + 1) < darr->capacity) {
+    darr->arr[darr->size] = x;
+    darr->size++;
+    return 0;
+  }
+  fprintf(stderr, "At capacity\n");
+  return 1;
 }
 
 int main(void) {
-  printf("Hello, world\n");
+  Darray_int darr = Darray_int_init(5);
+  Darray_int_print(&darr);
+  Darray_int_push(&darr, 1);
+  Darray_int_print(&darr);
+  Darray_int_destroy(&darr);
   return 0;
 }
