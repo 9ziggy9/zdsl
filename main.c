3e8ce920a8ddf415041a2bbc719c35d2032ed680
9ziggy9 <davidarogers@protonmail.com>
2022-07-28 09:55:05 -0700

    dynamic resizing implemented

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,8 +16,19 @@ Darray_int Darray_int_init(size_t capacity) {
   return darr;
 }
 
+void Darray_int_grow(Darray_int* darr) {
+  assert(darr->capacity == darr->size &&
+	 "should not grow array of sane size");
+  if (darr->capacity == darr->size) {
+    darr->capacity *= 2;
+    darr->arr = realloc(darr->arr, darr->capacity * sizeof(int));
+  }
+}
+
 void Darray_int_destroy(Darray_int* darr) {
   free(darr->arr);
+  darr->arr = NULL;
+  darr->size = darr->capacity = 0;
 }
 
 void Darray_int_print(Darray_int* darr) {
@@ -29,12 +41,12 @@ void Darray_int_print(Darray_int* darr) {
 }
 
 int Darray_int_push(Darray_int* darr, int x) {
-  if ((darr->size + 1) < darr->capacity) {
+  if ((darr->size + 1) <= darr->capacity) {
     darr->arr[darr->size] = x;
     darr->size++;
     return 0;
   }
-  fprintf(stderr, "At capacity\n");
+  Darray_int_grow(darr);
   return 1;
 }
 
@@ -45,6 +57,16 @@ int main(void) {
   Darray_int_push(&darr, 1);
   Darray_int_push(&darr, 1);
   Darray_int_push(&darr, 1);
+  Darray_int_push(&darr, 1);
+  Darray_int_push(&darr, 1);
+  Darray_int_push(&darr, 1);
+  Darray_int_push(&darr, 1);
+  Darray_int_push(&darr, 1);
+  Darray_int_push(&darr, 1);
+  Darray_int_push(&darr, 1);
+  Darray_int_push(&darr, 1);
+  Darray_int_push(&darr, 1);
+  Darray_int_print(&darr);
   Darray_int_destroy(&darr);
   return 0;
 }
